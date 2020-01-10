e838df998f2c05b4b53530818852df6f708aee7b
dangducton <dangducton@gmail.com>
2020-01-10 13:46:27 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'springbootangular';
+  title = 'Welcome to Client:Angular Sever:SpringBoot Cassandra NoSQL!';
 }
